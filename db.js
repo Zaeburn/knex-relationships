e835327d8f7529265dd5186521802fdd99cb73be
Zaeburn Mercer <zaeburn@outlook.com>
2018-04-04 18:02:17 +1200

    write getProfile function that joins tables

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ const connection = require('knex')(config)
 
 module.exports = {
   getUser: getUser,
-  getUsers: getUsers
+  getUsers: getUsers,
+  getProfile: getProfile
 }
 
 function getUsers (testConn) {
@@ -15,4 +16,16 @@ function getUsers (testConn) {
 function getUser (id, testConn) {
   const conn = testConn || connection
   return conn('users').where('id', id).first()
+}
+
+function getProfile (id, conn = connection) {
+  return conn('profiles')
+    .join('users', 'users.id', '=', 'profiles.user_id')
+    .select(
+      'profiles.user_id as profileUserID',
+      'profiles.website_url as webSiteUrl',
+      'profiles.avatar_url as avatarUrl',
+      'profiles.age',
+      'profiles.location'
+    )
 }

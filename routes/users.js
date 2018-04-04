80b4ddc0bc21bc6020ff888c3a5ca1896e5d6f18
Zaeburn Mercer <zaeburn@outlook.com>
2018-04-04 12:07:22 +1200

    create a profile page and /profile/id get route

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,8 @@ const router = express.Router()
 router.get('/', (req, res) => {
   db.getUsers()
     .then(users => {
+      console.log('users: ', users)
+      console.log('usersViewData: ', {users: users})
       res.render('index', { users: users })
     })
     .catch(err => {
@@ -14,5 +16,17 @@ router.get('/', (req, res) => {
     })
 })
 
-module.exports = router
+router.get('/profile/:id', (req, res) => {
+  const id = Number(req.params.id)
+  db.getUser(id)
+    .then(user => {
+      console.log('user: ', user)
+      console.log('userViewData: ', {user: user})
+      res.render('profile', user)
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
 
+module.exports = router

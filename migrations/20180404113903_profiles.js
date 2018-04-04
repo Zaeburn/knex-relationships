14cf5924db68a14b2b8fc10ed21c687b5a764e2b
Zaeburn Mercer <Zaeburn@outlook.com>
2018-04-04 20:03:32 +1200

    add random age function to profile seed data

--- migrations/20180404113903_profiles.js
+++ migrations/20180404113903_profiles.js
@@ -2,9 +2,9 @@ exports.up = (knex, Promise) => {
   return knex.schema.createTable('profiles', table => {
     table.increments('id').primary()
     table.integer('user_id').references('users.id')
+    table.integer('age')
     table.string('website_url')
     table.string('avatar_url')
-    table.integer('age')
     table.string('location')
   })
 }
--- seeds/profiles.js
+++ seeds/profiles.js
@@ -2,32 +2,43 @@ exports.seed = (knex, Promise) => {
   return knex('profiles').del()
     .then(function () {
       return knex('profiles').insert([
-        {user_id: 99901, website_url: 'aardvark.example.org'},
-        {user_id: 99902, website_url: 'baboon.example.org'},
-        {user_id: 99903, website_url: 'capybara.example.org'},
-        {user_id: 99904, website_url: 'duck.example.org'},
-        {user_id: 99905, website_url: 'elephant.example.org'},
-        {user_id: 99906, website_url: 'flying.fox.example.org'},
-        {user_id: 99907, website_url: 'gila.monster.example.org'},
-        {user_id: 99908, website_url: 'heron.example.org'},
-        {user_id: 99909, website_url: 'impala.example.org'},
-        {user_id: 99910, website_url: 'jerboa.example.org'},
-        {user_id: 99911, website_url: 'kinkajou.example.org'},
-        {user_id: 99912, website_url: 'lemur.example.org'},
-        {user_id: 99913, website_url: 'mongoose.example.org'},
-        {user_id: 99914, website_url: 'nyala.example.org'},
-        {user_id: 99915, website_url: 'ocelot.example.org'},
-        {user_id: 99916, website_url: 'panda.example.org'},
-        {user_id: 99917, website_url: 'quokka.example.org'},
-        {user_id: 99918, website_url: 'rail.example.org'},
-        {user_id: 99919, website_url: 'sloth.example.org'},
-        {user_id: 99920, website_url: 'terrapin.example.org'},
-        {user_id: 99921, website_url: 'urial.example.org'},
-        {user_id: 99922, website_url: 'viscacha.example.org'},
-        {user_id: 99923, website_url: 'wombat.example.org'},
-        {user_id: 99924, website_url: 'xerus.example.org'},
-        {user_id: 99925, website_url: 'yak.example.org'},
-        {user_id: 99926, website_url: 'zebu.example.org'}
+        {user_id: 99901, age: getRandomAge(18, 85) , website_url: 'aardvark.example.org'},
+        {user_id: 99902, age: getRandomAge(18, 85) , website_url: 'baboon.example.org'},
+        {user_id: 99903, age: getRandomAge(18, 85) , website_url: 'capybara.example.org'},
+        {user_id: 99904, age: getRandomAge(18, 85) , website_url: 'duck.example.org'},
+        {user_id: 99905, age: getRandomAge(18, 85) , website_url: 'elephant.example.org'},
+        {user_id: 99906, age: getRandomAge(18, 85) , website_url: 'flying.fox.example.org'},
+        {user_id: 99907, age: getRandomAge(18, 85) , website_url: 'gila.monster.example.org'},
+        {user_id: 99908, age: getRandomAge(18, 85) , website_url: 'heron.example.org'},
+        {user_id: 99909, age: getRandomAge(18, 85) , website_url: 'impala.example.org'},
+        {user_id: 99910, age: getRandomAge(18, 85) , website_url: 'jerboa.example.org'},
+        {user_id: 99911, age: getRandomAge(18, 85) , website_url: 'kinkajou.example.org'},
+        {user_id: 99912, age: getRandomAge(18, 85) , website_url: 'lemur.example.org'},
+        {user_id: 99913, age: getRandomAge(18, 85) , website_url: 'mongoose.example.org'},
+        {user_id: 99914, age: getRandomAge(18, 85) , website_url: 'nyala.example.org'},
+        {user_id: 99915, age: getRandomAge(18, 85) , website_url: 'ocelot.example.org'},
+        {user_id: 99916, age: getRandomAge(18, 85) , website_url: 'panda.example.org'},
+        {user_id: 99917, age: getRandomAge(18, 85) , website_url: 'quokka.example.org'},
+        {user_id: 99918, age: getRandomAge(18, 85) , website_url: 'rail.example.org'},
+        {user_id: 99919, age: getRandomAge(18, 85) , website_url: 'sloth.example.org'},
+        {user_id: 99920, age: getRandomAge(18, 85) , website_url: 'terrapin.example.org'},
+        {user_id: 99921, age: getRandomAge(18, 85) , website_url: 'urial.example.org'},
+        {user_id: 99922, age: getRandomAge(18, 85) , website_url: 'viscacha.example.org'},
+        {user_id: 99923, age: getRandomAge(18, 85) , website_url: 'wombat.example.org'},
+        {user_id: 99924, age: getRandomAge(18, 85) , website_url: 'xerus.example.org'},
+        {user_id: 99925, age: getRandomAge(18, 85) , website_url: 'yak.example.org'},
+        {user_id: 99926, age: getRandomAge(18, 85) , website_url: 'zebu.example.org'}
       ])
     })
 }
+
+function getRandomAge(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+/* function randomLocation() {
+
+}
+ */
